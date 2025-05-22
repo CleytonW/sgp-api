c0f26bddea66b9d1a68307c3330593a64a65214c
Cleyton Melo <cleytonw11@gmail.com>
2025-05-22 00:09:14 -0300

    Finalizando método de atualizar tarefa

--- src/main/java/com/sgp/api/controller/ProjetoController.java
+++ src/main/java/com/sgp/api/controller/ProjetoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +22,6 @@ import com.sgp.api.service.ProjetoService;
 @RequestMapping("/projetos")
 public class ProjetoController {
 
-    @Autowired
-    private ProjetoService projetoService;
-
     @GetMapping
     public ResponseEntity<List<Projeto>> listarProjetos() {
         return ResponseEntity.ok().body(projetoService.carregarProjetos());
@@ -33,19 +31,18 @@ public class ProjetoController {
     public ResponseEntity<Optional<Projeto>> buscarProjetoPeloId(@PathVariable("id") Long id) {
         Optional<Projeto> projeto = projetoService.carregarDadosProjetoPeloId(id);
 
-        if(projeto.isEmpty()) {
+        if (projeto.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
         return ResponseEntity.ok().body(projeto);
     }
 
-
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> excluirProjeto(@PathVariable("id") Long id) {
         Optional<Projeto> projeto = projetoService.carregarDadosProjetoPeloId(id);
 
-        if(projeto.isEmpty()) {
+        if (projeto.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
@@ -57,7 +54,21 @@ public class ProjetoController {
     @PostMapping
     public ResponseEntity<Projeto> cadastrarProjeto(@RequestBody Projeto projeto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(projetoService.salvarProjeto(projeto));
-
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Projeto> atualizarProjeto(@PathVariable("id") Long id, @RequestBody Projeto dadosProjeto) {
+        Optional<Projeto> projeto = projetoService.carregarDadosProjetoPeloId(id);
+
+        if (projeto.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        dadosProjeto.setId(id);
+
+        return ResponseEntity.ok().body(projetoService.salvarProjeto(dadosProjeto));
+    }
+
+    @Autowired
+    private ProjetoService projetoService;
 }
